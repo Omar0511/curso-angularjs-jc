191e398a4c62f4bb59a2fdc23672f8412b8f7243
Omar Garcia <omar.garcia@morsa.com.mx>
2024-10-08 22:44:04 -0600

    App Cuaderno

---
+++ js/02.appCuaderno.js
@@ -0,0 +1,24 @@
+function AlumnosController($scope) {
+    $scope.alumnos = [
+        {
+            nombre: "Omar",
+            telefono: "1234567890",
+            curso: "angular.js"
+        },
+        {
+            nombre: "Sofia",
+            telefono: "9876543210",
+            curso: "java"
+        },
+        {
+            nombre: "Luis",
+            telefono: "0987654321",
+            curso: "php"
+        },
+        {
+            nombre: "Juan",
+            telefono: "7654321098",
+            curso: "javascript"
+        }
+    ];
+}
