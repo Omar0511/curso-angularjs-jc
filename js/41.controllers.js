516365e1af8bb35c630047eb478103ffb0a4cde3
Omar Garcia <omar.garcia@morsa.com.mx>
2025-01-07 22:59:47 -0600

    PROMISES

--- js/41.controllers.js
+++ js/41.controllers.js
@@ -4,5 +4,21 @@ angular.module('myApp.controllers', [])
     .controller('ListaPelisCtrl', ['$scope', 'rtmFactory', 
         function($scope) {
             $scope.countries = rtmFactory.getCountries();
+
+            $scope.loadMovies = function(countryCode) {
+                rtmFactory.getMovies(countryCode).then(function (response) {
+                        var movieArray = response.data.movies;
+
+                        console.log(JSON.stringify(movieArray));
+
+                        $scope.movies = movieArray;
+                    },
+                    function (response) {
+                        console.error('Error:', response.data.error);
+                    }
+                )
+            };
+
+            $scope.loadMovies('US'); // Load movies for the United States by default
         }
     ]);
