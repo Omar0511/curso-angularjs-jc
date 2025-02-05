0bec153e057ed810a1f86b9bb3945212989c1308
Omar Garcia <omar.garcia@morsa.com.mx>
2025-02-04 22:48:40 -0600

    Renderizando VIEWS, render

--- 53.CreandoWeb/app/controllers/index.server.controller.js
+++ 53.CreandoWeb/app/controllers/index.server.controller.js
@@ -1,3 +1,9 @@
 exports.render = function(req, res) {
-    res.send('Hola Mundo');
+    // res.send('Hola Mundo');
+
+    // Nombre plantilla (archivo sin la extensión: .ejs)
+    res.render('index', {
+        title: 'Express',
+        message: 'Hola Mundo'
+    });
 };
